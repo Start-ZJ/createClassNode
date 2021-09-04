5b57dfafc3d80208243c251ea4ddf5d82f390c24
zhangjun <847003171@qq.com>
2021-09-04 18:02:57 +0800

    第一次提交

---
+++ connect/connect.js
@@ -0,0 +1,18 @@
+// var mysql = require('mysql');
+// // 链接池：创建多个链接、复用与分发链接
+// exports.pool = mysql.createPool({
+//     connectionLimit: 20,
+//     host: 'localhost',
+//     port: '3366',
+//     user: 'root',
+//     password: '',
+//     database: 'sys'
+// })
+
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'note',
+//     port: '3366'
+// })
